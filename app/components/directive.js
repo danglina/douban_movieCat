1e882f4e4bc25a7d44e9b7dc4ddb380d03a7b4b3
linda <2295007136@qq.com>
2018-05-26 14:07:25 +0800

    第二次提交 实现directives的link的$scope.$watch监视效果

--- app/components/directive.js
+++ app/components/directive.js
@@ -4,23 +4,30 @@
 	mymodule.directive('autoFocus', ['$location',function($location) {
 		return {
 			restrict: 'A',
+
 			// 一般我们把，有关操作dom的放在link当中
 			link: function($scope, iElem, iAttrs,controller) {
 
-				// 这个是操作dom元素获取的地址
-				a_href = iElem.children().attr('href');
-				// 找到匹配的关键字
-				var type = a_href.replace(/#(\/.+?)\/\d+/,"$1");
-				var path = $location.path();
-				// 当a_href的匹配值与，地址开头一样，就添加类名
-				if(path.startsWith(type)){
-					iElem.addClass('active');
-				}
-				iElem.on('click',function () {
-					iElem.parent().children().removeClass('active')
-					iElem.addClass('active');
-
+				// 这个里面相当，anguala的小模块，具有监视功能能
+				$scope.location = $location;
+				// 监视页面地址的变化
+				$scope.$watch('location.path()',function (now,old) {
+					var a_href = iElem.children().attr('href');
+					// 找到匹配的关键字
+					var type = a_href.replace(/#(\/.+?)\/\d+/,"$1");
+					// 当a_href的匹配值与，地址开头一样，就添加类名
+					if(now.startsWith(type)){
+						iElem.parent().children().removeClass('active')
+						iElem.addClass('active');
+					}
 				})
+
+
+				// iElem.on('click',function () {
+				// 	iElem.parent().children().removeClass('active')
+				// 	iElem.addClass('active');
+                //
+				// })
 			}
 		};
 	}]);
